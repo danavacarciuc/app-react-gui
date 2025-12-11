e6a29264f35854ba99e76154ce187badf4ff0517
danavacarciuc <danavacarciuc.vd@gmail.com>
2025-12-11 19:38:12 +0200

    upd2

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -5,13 +5,20 @@ function UserDetails({ user }) {
     return <div className="details-container placeholder">Select a user to see details...</div>;
 
   return (
-    <div className="details-container">
-      <h2>User Details</h2>
-      <p><strong>Username:</strong> {user.username}</p>
-      <p><strong>Full Name:</strong> {user.fullName}</p>
-      <p><strong>Type:</strong> {user.type}</p>
-    </div>
+    user ? (
+      <div>
+        <h2>User Details</h2>
+        <p><strong>Username:</strong> {user.username}</p>
+        <p><strong>Full Name:</strong> {user.fullName}</p>
+        <p><strong>Type:</strong> {user.type}</p>
+      </div>
+    ) : (
+      <div style={{ fontSize: "22px", opacity: 0.7 }}>
+        Select a user to see details…
+      </div>
+    )
   );
+  
 }
 
 export default UserDetails;
