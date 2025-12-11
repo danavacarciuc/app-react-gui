24d9aa352a7af5e62e0839dfadb93f5f5f93c8f4
danavacarciuc <danavacarciuc.vd@gmail.com>
2025-12-11 18:57:20 +0200

    Fix SERVER backend URL

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -2,16 +2,20 @@ import { useEffect, useState } from "react";
 import User from "./User";
 import "./UserList.css";
 
-const SERVER = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
+const SERVER = "https://app-react-7pu7.onrender.com"; // FIX CORENT
 
 function UserList(props) {
   const [users, setUsers] = useState([]);
-  const [selected, setSelected] = useState(null); // FIX 1
+  const [selected, setSelected] = useState(null);
 
   const getUsers = async () => {
-    const response = await fetch(`${SERVER}/users`);
-    const data = await response.json();
-    setUsers(data);
+    try {
+      const response = await fetch(`${SERVER}/users`);
+      const data = await response.json();
+      setUsers(data);
+    } catch (err) {
+      console.error("Error loading users:", err);
+    }
   };
 
   useEffect(() => {
@@ -22,8 +26,6 @@ function UserList(props) {
     <div className="user-list">
       {users.map((u) => (
         <div key={u.id} onClick={() => props.onSelectUser(u)}>
-          {" "}
-          {/* FIX 2 */}
           <User item={u} />
         </div>
       ))}
