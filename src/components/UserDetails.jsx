5b71357d4ab65717db400a1cc88bc5ca5d049e29
danavacarciuc <danavacarciuc.vd@gmail.com>
2025-12-11 19:28:13 +0200

    upd

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,27 +1,15 @@
-import "./User.css";
+import "./UserDetails.css";
 
 function UserDetails({ user }) {
-  if (!user) {
-    return <div className="details">Select a user to see details...</div>;
-  }
+  if (!user)
+    return <div className="details-container placeholder">Select a user to see details...</div>;
 
   return (
-    <div className="details">
+    <div className="details-container">
       <h2>User Details</h2>
-      <p>
-        <strong>Username:</strong> {user.username}
-      </p>
-      <p>
-        <strong>Full Name:</strong> {user.fullName}
-      </p>
-      {user.email && (
-        <p>
-          <strong>Email:</strong> {user.email}
-        </p>
-      )}
-      <p>
-        <strong>Type:</strong> {user.type}
-      </p>
+      <p><strong>Username:</strong> {user.username}</p>
+      <p><strong>Full Name:</strong> {user.fullName}</p>
+      <p><strong>Type:</strong> {user.type}</p>
     </div>
   );
 }
